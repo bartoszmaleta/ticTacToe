ecfe570d8be0a9c566f0a3e484f51b1cf2b07795
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-05-13 10:04:49 +0200

    Add placing mark X

--- public/js/game.js
+++ public/js/game.js
@@ -1,13 +1,26 @@
+const X_CLASS = 'x';
+const CIRCLE_CLASS = 'circle';
 const cellElements = document.querySelectorAll('[data-cell');
+let circleTurn;
+
 
 cellElements.forEach(cell => {
-    cell.addEventListener('click', hadnleClick, { once: true})
+    cell.addEventListener('click', handleClick, { once: true})
 });
 
 function handleClick(e) {
     console.log('clicked');
+    const cell = e.target;
+    const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
+    placeMark(cell, currentClass);
+
     // Place mark
     // Check for win
     // Check for Draw
     // Switch Turns
+}
+
+function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass);
+
 }
