2289d70e21bc81424ed1334af292bfb6f5745a0c
StaNC2000 <stanislaw.chynek@gmail.com>
2020-05-13 14:59:37 +0200

    Winning possibilities array added

--- public/js/script.js
+++ public/js/script.js
@@ -2,6 +2,21 @@ const playerX = 'player-x';
 const playerO = 'player-o';
 
 let currentPlayer = playerX;
+const buttons = document.querySelectorAll(".box");
+
+const winningOptions = [
+    ['top-left','top-center','top-right'],
+    ['mid-left','mid-center','mid-right'],
+    ['bottom-left', 'bottom-center','bottom-right'],
+    ['top-left', 'mid-left','bottom-left'],
+    ['top-center', 'mid-center','bottom-center'],
+    ['top-right','mid-right', 'bottom-right'],
+    ['top-right', 'mid-center', 'bottom-left'],
+    ['top-left', 'mid-center', 'bottom-right']];
+
+let playerClicks = [];
+
+let computerClicks = [];
 
 function switchPlayers() {
     if (currentPlayer === playerX) {
@@ -13,8 +28,6 @@ function switchPlayers() {
     }
 }
 
-const buttons = document.querySelectorAll(".box");
-
 for (let i = 0; i < buttons.length; i++) {
     let button = buttons[i];
     if (button.firstElementChild.className === '') {
@@ -22,7 +35,14 @@ for (let i = 0; i < buttons.length; i++) {
             let currentBox = button.firstElementChild;
             currentBox.setAttribute("class", switchPlayers());
             console.log(this.firstElementChild.className);
-
+            console.log(this.getAttribute('id'));
+            console.log(switchPlayers());
+            if(switchPlayers() === playerO){
+                playerClicks.push(this.getAttribute('id'));
+            }
+            else {
+                computerClicks.push(this.getAttribute('id'));
+            }
             button.removeEventListener('click', updateButtonStatus);
         }
 
