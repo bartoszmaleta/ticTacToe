05843fcdaa6f8824d608dc8731a744bb3106e344
StaNC2000 <stanislaw.chynek@gmail.com>
2020-05-14 08:48:56 +0200

    Initial commit for new branch

--- public/js/script.js
+++ public/js/script.js
@@ -1,21 +1,17 @@
 const playerX = 'player-x';
 const playerO = 'player-o';
-
 let currentPlayer = playerX;
 const buttons = document.querySelectorAll(".box");
-
 const winningOptions = [
-    ['top-left','top-center','top-right'],
-    ['mid-left','mid-center','mid-right'],
-    ['bottom-left', 'bottom-center','bottom-right'],
-    ['top-left', 'mid-left','bottom-left'],
-    ['top-center', 'mid-center','bottom-center'],
-    ['top-right','mid-right', 'bottom-right'],
+    ['top-left', 'top-center', 'top-right'],
+    ['mid-left', 'mid-center', 'mid-right'],
+    ['bottom-left', 'bottom-center', 'bottom-right'],
+    ['top-left', 'mid-left', 'bottom-left'],
+    ['top-center', 'mid-center', 'bottom-center'],
+    ['top-right', 'mid-right', 'bottom-right'],
     ['top-right', 'mid-center', 'bottom-left'],
     ['top-left', 'mid-center', 'bottom-right']];
-
 let playerClicks = [];
-
 let computerClicks = [];
 
 function switchPlayers() {
@@ -28,28 +24,38 @@ function switchPlayers() {
     }
 }
 
-for (let i = 0; i < buttons.length; i++) {
-    let button = buttons[i];
-    if (button.firstElementChild.className === '') {
-        function updateButtonStatus() {
-            let currentBox = button.firstElementChild;
-            currentBox.setAttribute("class", switchPlayers());
-            console.log(this.firstElementChild.className);
-            console.log(this.getAttribute('id'));
-            console.log(switchPlayers());
-            if(switchPlayers() === playerO){
-                playerClicks.push(this.getAttribute('id'));
+function markBox() {
+    for (let i = 0; i < buttons.length; i++) {
+        let button = buttons[i];
+        if (button.firstElementChild.className === '') {
+            function updateButtonStatus() {
+                let currentBox = button.firstElementChild;
+                currentBox.setAttribute("class", switchPlayers());
+                console.log(this.firstElementChild.className);
+                console.log(this.getAttribute('id'));
+                console.log(switchPlayers());
+                if (switchPlayers() === playerO) {
+                    playerClicks.push(this.getAttribute('id'));
+                } else {
+                    computerClicks.push(this.getAttribute('id'));
+                }
+                button.removeEventListener('click', updateButtonStatus);
             }
-            else {
-                computerClicks.push(this.getAttribute('id'));
-            }
-            button.removeEventListener('click', updateButtonStatus);
+            button.addEventListener('click', updateButtonStatus);
+            // button.document.classList="player-x";
         }
+    };
+}
 
-        button.addEventListener('click', updateButtonStatus);
-        // button.document.classList="player-x";
-    }
-};
+markBox();
+
+function checkWin(currentClass) {
+    return winningOptions.some(combination => {
+        return combination.every(index => {
+            return cellElements[index].classList.contains(currentClass);
+        })
+    })
+}
 
 // TODO: find all buttons *
 //
